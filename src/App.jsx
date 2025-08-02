acb64c8fa53b12c3d51c89f6ffd557152fa19f16
Ivann <ivanpaglinawan0@gmail.com>
2025-08-02 16:10:59 +0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       {open ? <Modal deleteItem = {() => setDeleteItem(false)} onClose={() => setOpen(false)}/> : 
         <div className='d-flex flex-column align-items-center gap-2'>
           <span>This is a custom modal</span>
-          <span>Click "Delete" to open Modal</span>
+          {deleteItem ? <span>Click "Delete" to open Modal</span> : <span>No item found!</span>}
           {deleteItem ? <button className='btn btn-danger gap-2 d-flex' onClick={onDelete}><i className='bi bi-trash'></i>Delete</button> : ""}
         </div>}
     </div>
